054ddb174c781884380b6f420ad3de42fe0c5037
teichx <teichx@hotmail.com>
2023-08-18 00:57:51 -0300

    refactor: Improve GRPCExtension

--- CodePool/CodePool.Sharp/Extension/GRPCExtension.cs
+++ CodePool/CodePool.Sharp/Extension/GRPCExtension.cs
@@ -3,12 +3,28 @@ using ProtoBuf.Grpc.Reflection;
 
 namespace CodePool.Sharp.Extension;
 
+/// <summary>
+/// The class <c>GRPCExtension</c> provides the abstractions related to grpc
+/// </summary>
 public static class GRPCExtension
 {
+    public const string ProtoFolder = "proto";
+
+    private static void EnsureCleanupDirectoryExists()
+    {
+        var directory = Directory.CreateDirectory(ProtoFolder);
+        directory.Delete(recursive: true);
+        directory.Create();
+    }
+
+    /// <summary>
+    /// Generates all .proto files in default folder.
+    /// </summary>
     public static void GenerateProto()
     {
-        File.WriteAllText("proto/.gitignore", "*.*");
+        EnsureCleanupDirectoryExists();
 
+        File.WriteAllText(Path.Join(ProtoFolder, ".gitignore"), "*.*");
         var generator = new SchemaGenerator();
 
         AssemblyWalker.InterfacesWithAttribute<ServiceAttribute>()
@@ -18,7 +34,7 @@ public static class GRPCExtension
                 var fileName = inheritanceType.FullName;
                 var schema = generator.GetSchema(inheritanceType);
 
-                File.WriteAllText($"proto/{fileName}.proto", schema);
+                File.WriteAllText(Path.Join(ProtoFolder, $"{fileName}.proto"), schema);
             });
     }
 }
