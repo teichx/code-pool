3996ab42b556086dfa3fe9eae1704b6f5c89ac24
teichx <teichx@hotmail.com>
2023-08-21 07:37:35 -0300

    fix: Program start dependency

--- CodePool/CodePool.Location/Program.cs
+++ CodePool/CodePool.Location/Program.cs
@@ -1,5 +1,4 @@
 using ProtoBuf.Grpc.Server;
-using CodePool.Location.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +7,6 @@ builder.Services.AddCodeFirstGrpcReflection();
 
 var app = builder.Build();
 
-app.MapGrpcService<LocationService>();
 app.MapCodeFirstGrpcReflectionService();
 
 app.Run();
