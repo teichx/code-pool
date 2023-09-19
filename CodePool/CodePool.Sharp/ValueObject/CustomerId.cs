9a8eef952ba4ed8129c00a2aad0a5cdde4752706
teichx <teichx@hotmail.com>
2023-09-19 20:32:02 -0300

    feat: CustomerId convert to int on serializations

--- CodePool/CodePool.Sharp/ValueObject/CustomerId.cs
+++ CodePool/CodePool.Sharp/ValueObject/CustomerId.cs
@@ -1,8 +1,12 @@
 using System.Globalization;
+using System.Text.Json.Serialization;
+using CodePool.Sharp.Serializer;
+using CodePool.Sharp.Serializer.Interfaces;
 
 namespace CodePool.Sharp.ValueObject;
 
-public readonly struct CustomerId
+[JsonConverter(typeof(SerializeToInt.IntJsonConverter))]
+public readonly struct CustomerId : IConvertibleToInt
 {
     public readonly int Id { get; }
     private CustomerId(int id)
@@ -12,10 +16,17 @@ public readonly struct CustomerId
         => Id.ToString(CultureInfo.InvariantCulture);
 
     public override int GetHashCode() => Id.GetHashCode();
+    public int ToInt() => Id;
 
     public static implicit operator int(CustomerId customerId) => customerId.Id;
     public static implicit operator CustomerId(int id) => new(id);
 
     static readonly Lazy<CustomerId> _empty = new(() => new(0));
     public static CustomerId Empty => _empty.Value;
+}
+
+[JsonSourceGenerationOptions]
+[JsonSerializable(typeof(CustomerId))]
+sealed partial class CustomerIdContext : JsonSerializerContext
+{
 }
